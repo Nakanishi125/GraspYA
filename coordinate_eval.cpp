e1c81c576422d3479ffb85e1c00cd3ee6635be92
Nakanishi125 <nakanishi.y125@gmail.com>
2020-12-25 01:30:17 +0900

    generalize coordinate function

--- coordinate_eval.cpp
+++ coordinate_eval.cpp
@@ -15,16 +15,20 @@ float coord_eval(dhFeaturePoints* Fp){
 //    QString fpname[] = {"16-a","16-b","16-c","02-a","02-b","02-c","05-a","05-b","05-c",       //母指側面利用
 //                        "11-a","11-b","11-c","08-a","08-b","08-c",
 //                        };
-    QString fpname[] = {"00-a","00-b","00-c","02-a","02-b","02-c","05-a","05-b","05-c",      //母指腹利用
-                        "11-a","11-b","11-c","08-a","08-b","08-c",
-                        };
+//    QString fpname[] = {"00-a","00-b","00-c","02-a","02-b","02-c","05-a","05-b","05-c",      //母指腹利用
+//                        "11-a","11-b","11-c","08-a","08-b","08-c",
+//                        };
+
+
+    QStringList fpname = Fp->allPointNames();
 
     const int L = 180;
 
-// ********************************************************************************************
-//ObjectPoints3.csvの読み込み
-    string list = "C:\\kenkyu\\GraspYA\\data\\ObjectPoints3.csv";
-    vector<vector<string>> ObjPs;       //これにデータ代入
+// ===========================
+// ObjectPoints3.csvの読み込み
+// ==========================
+    QString list = "C:\\kenkyu\\GraspYA\\data\\ObjectPoints3.csv";
+    vector<vector<QString>> ObjPs;       //これにデータ代入
 
     Csv Obj(list);
     if(!Obj.getCsv(ObjPs)){
@@ -43,10 +47,11 @@ float coord_eval(dhFeaturePoints* Fp){
             cout << endl;
     } */
 
-// ********************************************************************************************
+//==============================
 //ObjectNormalvecs3.csvの読み込み
-    string list_normal = "C:\\kenkyu\\GraspYA\\data\\ObjectNormalvecs3.csv";
-    vector<vector<string>> ObjPs_normal;    //これにデータ代入
+//==============================
+    QString list_normal = "C:\\kenkyu\\GraspYA\\data\\ObjectNormalvecs3.csv";
+    vector<vector<QString>> ObjPs_normal;    //これにデータ代入
 
     Csv Obj_normal(list_normal);
     if(!Obj_normal.getCsv(ObjPs_normal)){
@@ -69,49 +74,54 @@ float coord_eval(dhFeaturePoints* Fp){
 
     double sum_length = 0;
     double sum_rad = 0;
+    QStringList finger;
 
     for(int i=0;i<ObjPs.size();i++){
-        dhVec3 ops_forhand(stod(ObjPs[i][1]),stod(ObjPs[i][2]),stod(ObjPs[i][3]));
-        dhVec3 ops_normal_forhand(stod(ObjPs_normal[i][1]),stod(ObjPs_normal[i][2]),stod(ObjPs_normal[i][3]));
+        dhVec3 ops_forhand(ObjPs[i][1].toDouble(),ObjPs[i][2].toDouble(),ObjPs[i][3].toDouble());
+        dhVec3 ops_normal_forhand(ObjPs_normal[i][1].toDouble(),ObjPs_normal[i][2].toDouble(),ObjPs_normal[i][3].toDouble());
         double length,rad,estimate;
 
+        for(int index=0; index<fpname.size(); index++){
+            finger = fpname.filter(ObjPs[i][0]);
 
-        for(int j=0;j<3;j++){
-            dhFeaturePoint* np = Fp -> point(fpname[3*i+j]);    //特定のFeaturePointを指定
+            for(int j=0; j<finger.size(); j++){
+                dhFeaturePoint* np = Fp -> point(finger.value(j));        //特定のFeaturePointを指定
 
-            dhVec4 rps_forobj2 = np -> position();              //選んだFeaturePointの位置座標取得
-            dhVec4 rps_normal_forobj2 = np -> normal();         //選んだFeaturePointのベクトル取得
+                dhVec4 rps_forobj2 = np -> position();              //選んだFeaturePointの位置座標取得
+                dhVec4 rps_normal_forobj2 = np -> normal();         //選んだFeaturePointのベクトル取得
 
-            dhVec3 rps_forobj = rps_forobj2.toVec3();           //dhVec4 -> dhVec3
-            dhVec3 rps_normal_forobj = rps_normal_forobj2.toVec3();
+                dhVec3 rps_forobj = rps_forobj2.toVec3();
+                dhVec3 rps_normal_forobj = rps_normal_forobj2.toVec3();
 
-            double length_can,rad_can,estimate_can;
+                double length_can,rad_can,estimate_can;
 
-//            DH_LOG("ops_forhand is "+QString::number(ops_forhand[0],'f',5),0);
-//            DH_LOG("rps_forobj is "+QString::number(rps_forobj[0],'f',5),0);
+//                DH_LOG("ops_forhand is "+QString::number(ops_forhand[0],'f',5),0);
+//                DH_LOG("rps_forobj is "+QString::number(rps_forobj[0],'f',5),0);
 
-            length_can = (rps_forobj - ops_forhand).norm()/L;       //距離の評価式
+                length_can = (rps_forobj - ops_forhand).norm()/L;       //距離の評価式
 
-            double cost,theta_can;
-            cost = (rps_normal_forobj * ops_normal_forhand)/(rps_normal_forobj.norm() * ops_normal_forhand.norm());
-            theta_can = acos(cost);
-            rad_can = (PI - theta_can)/PI;                          //角度の評価式
+                double cost,theta_can;
+                cost = (rps_normal_forobj * ops_normal_forhand)/(rps_normal_forobj.norm() * ops_normal_forhand.norm());
+                theta_can = acos(cost);
+                rad_can = (PI - theta_can)/PI;                          //角度の評価式
 
-            estimate_can = length_can + rad_can;
+                estimate_can = length_can + rad_can;
 
 
-            if(j == 0){
-                estimate = estimate_can;
-                length = length_can;
-                rad = rad_can;
-            }
-            else{
-                if(estimate > estimate_can){
+                if(j == 0){
                     estimate = estimate_can;
                     length = length_can;
                     rad = rad_can;
                 }
+                else{
+                    if(estimate > estimate_can){
+                        estimate = estimate_can;
+                        length = length_can;
+                        rad = rad_can;
+                    }
+                }
             }
+            finger.clear();
         }
 
         sum_length += length;
